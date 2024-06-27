84fb40966a2243b0d5580d0ea811970129dd71cd
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-26 18:35:10 -0700

    hw3-2

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
     ########################################
     """
     # set minval
-    if num1 > num2 and 
+    if num1 < num2 and num1 < num3:
+        minval = num1        
+    elif num2 < num1 and num2 < num3:
+        minval = num2    
+    else: minval = num3
     
     # set midian
     
